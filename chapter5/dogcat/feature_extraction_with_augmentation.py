2bd9ff9b8e4f09242e2afbfaaf59c871ccc39e48
java-chip <takuyam0111@yahoo.co.jp>
2019-05-22 15:34:30 +0900

    結果をプロット表示

--- chapter5/dogcat/feature_extraction_with_augmentation.py
+++ chapter5/dogcat/feature_extraction_with_augmentation.py
@@ -77,7 +77,28 @@ history = model.fit_generator(train_generator,
                               validation_steps=50,
                               verbose=2)
 
+acc = history.history['acc']
+val_acc = history.history['val_acc']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
+epochs = range(len(acc))
+
+# 正解率をプロット
+plt.plot(epochs, acc, 'bo', label = 'Training acc')
+plt.plot(epochs, val_acc, 'b', label = 'Validation acc')
+plt.title('Training and validation accuracy')
+plt.legend()
+
+plt.figure()
+
+# 損失値をプロット
+plt.plot(epochs, loss, 'bo', label = 'Training loss')
+plt.plot(epochs, val_loss, 'b', label = 'Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+
+plt.show()
 
 
 
